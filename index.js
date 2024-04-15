8bcb09c4221d8c0b1fb173c510ced33b9b59ebb3
asseBadiane <badianeasse99@gmail.com>
2024-04-15 10:16:29 +0000

    ✨ Implements CRUD User and Product  🔧   config sqlite and nodemon

--- index.js
+++ index.js
@@ -2,36 +2,18 @@ const express = require('express')
 const app = express()
 const path = require('path')
 var bodyParser = require('body-parser')
-const db = require('./db')
 
 app.use(express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/users', function(req, res) {
-  
+const usersRoute = require('./routes/usersRoute')
+const productsRoute = require('./routes/productsRoute')
 
-  res.set('Content-Type', 'text/html')
-  res.set('Server', 'My Ecommerce Server')
-})
+app.use('/users', usersRoute)
+app.use('/products', productsRoute)
 
-app.post('/users', function (req, res) {
-  // const contentHtml = `
-  // <head>
-  //   <title>Welcome to My Ecommerce</title>
-  // </head>
-  // <body>
-  //   <h1>Hello World 😀</h1>
-  // </body>
-  // `
-  const data = req.body
-  console.log('data', data)
-
-  res.set('Content-Type', 'text/html')
-  res.set('Server', 'My Ecommerce Server')
-})
 
 app.listen(3000) 
-
 console.log('Server listening on port 3000 😎')
 
